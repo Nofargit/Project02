430103f4aa4404ee0421cee7f7c97f2e33ab8514
Nofargit <47120091+Nofargit@users.noreply.github.com>
2019-05-21 18:28:42 +0300

    Update aboutPage.js

--- ProjectCurrency/aboutPage.js
+++ ProjectCurrency/aboutPage.js
@@ -16,17 +16,16 @@
                 $("#aboutPage").html(
                ` 
                     <h3> About the Project: </h3>
-                This project includes live information for a variety crypto prices. <br>
-                here you can receive any information for your relevant currency and 
-                compare between couple of currencies in live report. 
+                This project includes information in live time for a variety of crypto prices. <br>
+                here you can receive any information for your relevant currency and get all your wanted coins in one live report !. 
                 </di>
-                
                 <br>
         
                 <h3> About me: </h3>
                 <div> 
                 My name is Nofar, I'm  26 years old and I'm a student for web developing.
-                My purpose in life is to finish this project !   
+                
+
                 </div>
                 <img class="image" src='https://i2.wp.com/www.women-zone.com/wp-content/uploads/2017/06/wonder_woman_main.jpg?fit=1280%2C640' >
             ` 
