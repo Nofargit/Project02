d1a90029fb1b72c1c0d24302263c30900c4dc01e
Nofargit <47120091+Nofargit@users.noreply.github.com>
2019-05-30 12:03:14 +0300

    Update aboutPage.js

--- ProjectCurrency/aboutPage.js
+++ ProjectCurrency/aboutPage.js
@@ -8,30 +8,28 @@
             $("#aboutPage").show()
             $("#content").hide()
 
-
-            
-          //  $("#content").html(`
-
-
                 $("#aboutPage").html(
                ` 
                     <h3> About the Project: </h3>
                 This project includes information in live time for a variety of crypto prices. <br>
-                here you can receive any information for your relevant currency and get all your wanted coins in one live report !. 
+                here you can receive any information for your relevant currency and get all your wanted coins in one live report!
+               
+
                 </di>
                 <br>
         
                 <h3> About me: </h3>
                 <div> 
-                My name is Nofar, I'm  26 years old and I'm a student for web developing.
-                
+                My name is Nofar, I'm  26 years old and I'm a student for web developing. I built this web so everyone will be able to invest their money on the best currency at the best time.
+                I believe that crypto currencies are the future and I recommend you to invest some of your money in order to make larger money! 
+               
+                                
 
                 </div>
                 <img class="image" src='https://i2.wp.com/www.women-zone.com/wp-content/uploads/2017/06/wonder_woman_main.jpg?fit=1280%2C640' >
             ` 
             )
-            // )
-        
+           
         return ;
         }
         
